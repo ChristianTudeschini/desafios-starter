4cff52a61311142b79661b48bda232aadcfeadb5
Christian Tudeschini <54380237+ChristianTudeschini@users.noreply.github.com>
2020-03-16 23:04:22 -0300

    exercicio 7

--- cursos/JavaScript ES6/Módulo 1/Exercício 7/bundle.js
+++ cursos/JavaScript ES6/Módulo 1/Exercício 7/bundle.js
@@ -1,17 +1,45 @@
 "use strict";
 
-var nome = 'Rocketseat';
-var cidade = 'Rio do Sul';
-var estado = 'SC';
-var empresa = {
-  nome: nome,
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// 5.1 = REST
+var arr = [1, 2, 3, 4, 5, 6];
+
+function soma(x) {
+  for (var _len = arguments.length, params = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    params[_key - 1] = arguments[_key];
+  }
+
+  return x + params.reduce(function (valorAtual, proxValor) {
+    return valorAtual + proxValor;
+  });
+}
+
+console.log(soma.apply(void 0, [arr[0]].concat(arr))); // 5.2 = SPREAD
+
+var usuario = {
+  nome: 'Diego',
+  idade: 23,
   endereco: {
-    cidade: cidade,
-    estado: estado
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
   }
 };
-console.log(nome); // Rocketseat
 
-console.log(cidade); // Rio do Sul
+var usuario2 = _objectSpread({}, usuario, {
+  nome: 'Gabriel'
+});
 
-console.log(estado); // SC
+var usuario3 = _objectSpread({}, usuario, {
+  endereco: {
+    cidade: 'Lontras'
+  }
+});
+
+console.log(usuario2);
+console.log(usuario3);
